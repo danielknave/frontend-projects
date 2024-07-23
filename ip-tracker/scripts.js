d6d286722467b6a4f9fcd1a6f960a63451b907dd
Danila Frolov <hotlinetheslayer@gmail.com>
2024-07-23 17:09:57 +0200

    ip data loading added

--- ip-tracker/scripts.js
+++ ip-tracker/scripts.js
@@ -3,11 +3,29 @@ function search() {
     if (!validate(input)) {
         alert('Incorrect IP');
         return;
-    }
-    
+    }  
+    const api_key = "at_Of04vvlo6VltwEQxf6PstIKx8OPF0";
+    const regionalNamesInEnglish = new Intl.DisplayNames(['en'], {type: 'region'});
+    $.ajax({
+        url: "https://geo.ipify.org/api/v1",
+        data: {apiKey: api_key, ipAddress: input},
+        success: function(data) {
+            document.querySelector('#ip .contents').innerText = input;
+
+            let locationElement = document.querySelector('#location .contents');
+            locationElement.innerText = data.location.city + ', ' + data.location.region + ', ' + regionalNamesInEnglish.of(data.location.country);
+
+            let timezoneElement = document.querySelector('#timezone .contents');
+            timezoneElement.innerText = 'UTC ' + data.location.timezone;
+
+            let ISPElement = document.querySelector('#isp .contents');
+            ISPElement.innerText = data.isp;
+        }
+    })
 }
 
 function validate(input) {
     const regex = new RegExp(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
     return regex.test(input);
 }
+
